2f423328538df86a21a5682a748ee2b3d1eb850f
Alex Brochu <alexbrochu1@gmail.com>
2026-03-11 21:01:02 -0700

    setup build system

--- build.bat
+++ build.bat
@@ -1,10 +1,15 @@
 @echo off
 
+set rflags=-nologo -v -Fo.\objects\resources.res
+set res-files=.\src\resources.rc
+
 set cflags=-nologo -std:c++17 -EHsc -Zi -MT -MP -WX -Fo.\objects\
 set includes=-I .\includes\
-set defines=-D"BUILD_WINDOWS=1"
-set src-files=.\src\main.cpp
+set defines=-D"BUILD_WINDOWS=1" -D"BUILD_DEBUG=1"
+set src-files=.\src\~unity.cpp
+
 set lflags=-NOLOGO -DEBUG -LIBPATH:.\libs\
 SET libs="kernel32.lib" "user32.lib" "gdi32.lib" "d3d12.lib" "dxguid.lib" "dxgi.lib" "d3dcompiler.lib"
 
-cl %cflags% %includes% %defines% %src-files% -link %lflags% %libs% -SUBSYSTEM:WINDOWS -OUT:rtbench.exe
+rc %rflags% %res-files%
+cl %cflags% %includes% %defines% %src-files% -link %lflags% .\objects\resources.res %libs% -SUBSYSTEM:WINDOWS -OUT:rtbench.exe
